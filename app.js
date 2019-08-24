6393b4b71364876927b0bd02bb275bf9182f33fa
Daison Cariño <daison@incube8.sg>
2019-08-25 00:24:11 +0800

    Remove async/await to make run it async and much faster; Add yarn lock for yarn lovers

--- app.js
+++ app.js
@@ -8,18 +8,14 @@ ROGUARD_CARD_PAGES = 15
 ROGUARD_EQUIPMENT_PAGES = 149
 ROGUARD_ITEM_PAGES = 254
 
-const start = async () => {
-  // // clone and generate monster data from ROGuard
-  await monsterService.clone(ROGUARD_MONSTER_PAGES)
+// clone and generate monster data from ROGuard
+monsterService.clone(ROGUARD_MONSTER_PAGES)
 
-  // // clone and generate card data from ROGuard
-  await cardService.clone(ROGUARD_CARD_PAGES)
+// clone and generate card data from ROGuard
+cardService.clone(ROGUARD_CARD_PAGES)
 
-  // // clone and generate equipment data from ROGuard
-  await equipmentService.clone(ROGUARD_EQUIPMENT_PAGES)
+// clone and generate equipment data from ROGuard
+equipmentService.clone(ROGUARD_EQUIPMENT_PAGES)
 
-  // clone and generate item data from ROGuard
-  await itemService.clone(ROGUARD_ITEM_PAGES)
-}
-
-start()
+// clone and generate item data from ROGuard
+itemService.clone(ROGUARD_ITEM_PAGES)
