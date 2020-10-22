2ac6107284117e8ea69b4e0d81f71f62edd04b10
THEBUSTED <tksumeth@gmail.com>
2020-10-22 15:43:07 +0700

    Update app.js
    eslint fix

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const cardService = require('./public/js/card-service')
 const equipmentService = require('./public/js/equipment-service')
 const itemService = require('./public/js/item.service')
 
-ROGUARD_MONSTER_PAGES = 12
-ROGUARD_CARD_PAGES = 15
-ROGUARD_EQUIPMENT_PAGES = 149
-ROGUARD_ITEM_PAGES = 254
+const ROGUARD_MONSTER_PAGES = 12
+const ROGUARD_CARD_PAGES = 15
+const ROGUARD_EQUIPMENT_PAGES = 149
+const ROGUARD_ITEM_PAGES = 254
 
 // clone and generate monster data from ROGuard
 monsterService.clone(ROGUARD_MONSTER_PAGES)
